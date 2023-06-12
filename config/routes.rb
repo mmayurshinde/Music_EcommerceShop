1a4a0a92b4ffe985766bed70611de4ab9661f6ad
sourabh patware <sourabhpatware100@gmail.com>
2023-06-12 17:05:55 +0530

    init

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users,controllers:{
+  	registration:'registrations'
+  }
+  get 'store/index'
+  root 'store#index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
